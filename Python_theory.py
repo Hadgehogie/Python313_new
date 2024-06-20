94053e1dc80298ad72414f96695d3778bc1403d0
Hadgehogie <test@mail.ru>
2024-06-20 12:01:56 +0300

    added more theory

--- Python_theory.py
+++ Python_theory.py
@@ -1505,4 +1505,140 @@
 
 # print("Some random changes")
 
-print("Cloned repository")
+# print("Cloned repository")
+
+
+# -----------------------------------------------------
+# Множества (set)
+# Значения не упорядочены, дубликаты не выводятся, нет индексов или ключей
+
+# s = {'banana', 'apple', 'orange', 'banana', 'apple'}
+# print(s)  # {'orange', 'banana', 'apple'}
+# print(s[0])  # TypeError: 'set' object is not subscriptable
+# print(type(s))
+# print(len(s))  # 3
+
+# a = {}
+# b = set('hello')
+# print(a, type(a))  # {} <class 'dict'>
+# print(b, type(b))  # {'l', 'h', 'e', 'o'} <class 'set'>
+
+# c = ['red', 'blue', 'green', 'red']
+# a = set(c)
+# print(a, type(a))
+
+# mas = [1, 2, 3, 2, 3, 4, 4, 5]
+# s = {x for x in mas if x % 2 == 0}
+# print(s)
+
+# def to_set(a):
+#     b = set(a)
+#     return b, len(b)
+#
+#
+# print(to_set('я обычная строка'))
+# print(to_set([4, 5, 4, 6, 2, 9, 11, 3, 4, 2]))
+
+# t = {'red', 'green', 'blue'}
+# # print('green' in t)  # True
+# for i in t:
+#     print(i)
+
+# r = ['ab_1', 'ac_2', 'bc_1', 'bc_2']
+# # a = [i for i in r if 'a' not in i]
+# # a = ['A' + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in r]
+# a = ['A' + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in r if i[1] == 'c']
+# print(a)
+
+# a = {'Tom', 'Bob', 'Alice'}
+# print(a)
+# a.add('Ann')
+# print(a)
+# a.remove('Ann')
+# # a.remove('Ann1')  # KeyError (обратились к несуществующему элементу)
+# print(a)
+# # user = 'Tom'
+# # if user in a:
+# #     a.remove(user)
+# # print(a)
+# # a.discard('Ann')  # = remove
+# # a.discard('Ann1')  # NO ERROR
+# # print(a)
+# n = a.pop()
+# print(a)
+# print(n)
+# a.clear()
+# print(a)  # set()
+
+# a = {0, 1, 2, 3}
+# b = {4, 3, 2, 1}
+# # c = a.union(b)  # {0, 1, 2, 3, 4}
+# # a |= b
+# c = a | b  # {0, 1, 2, 3, 4}
+# print(c)
+# # a &= b
+# d = a & b  # {1, 2, 3}
+# print(d)
+# # a -= b
+# e = a - b  # {0}
+# print(e)
+# # a ^= b
+# f = a ^ b
+# print(f)  # {0, 4}
+
+# a, b, c, d, e, f, g = {1, 2}, {3}, {4, 5}, {3, 2, 6}, {6}, {7, 8}, {9, 8}
+# unique = a | b | c | d | e | f | g
+# print(unique)
+# print('Unic elems count:', len(unique))
+# print('Min elem:', min(unique))
+# print('Max elem:', max(unique))
+# print('Sum elem:', sum(unique))
+
+# a = set(input('Введите первую строку: '))
+# b = set(input('Введите вторую строку: '))
+# c = a & b
+# print('Общими буквами являются:')
+# for i in c:
+#     print(i, end=' ')
+
+# a = set(input('Введите первую строку: '))
+# b = set(input('Введите вторую строку: '))
+# c = a - b
+# print('Искомыми буквами являются:')
+# for i in c:
+#     print(i, end=' ')
+
+# Drawing = {'Marina', 'Jenya', 'Sveta'}
+# Music = {'Kostya', 'Jenya', 'Ilya'}
+#
+# print('Only one hobby:', Drawing ^ Music)
+# print('Both hobbies:', Drawing & Music)
+# print('Drawing:', Drawing - Music)
+
+# a = {0, 1, 2, 3, 4}
+# b = {3, 2, 1}
+# print(a <= b)  # False
+# print(a >= b)  # True
+
+
+# -----------------------------------------------------
+# Тип замороженные множества (frozenset)
+
+# s = frozenset([1, 2, 3, 4, 5])
+# print(s)  # frozenset({1, 2, 3, 4, 5})
+# print(type(s))
+# a = frozenset({'hello', 'world'})
+# print(a)  # frozenset({'hello', 'world'})
+
+# a = [7, 4, 5, 6, 6, 8, 2, 3, 5, 8, 9, 1, 2, 4, 0, 8]
+# print(a)
+# b = set(a)
+# # print(b)
+# a = list(b)
+# c = tuple(b)
+# print(a)
+# print(c)
+
+
+# -----------------------------------------------------
+# Словари
