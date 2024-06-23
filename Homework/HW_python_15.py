3492c19ea8dd69a60904de316589732c8d8387e3
Hadgehogie <test@mail.ru>
2024-06-24 00:19:15 +0300

    added homework

---
+++ Homework/HW_python_15.py
@@ -0,0 +1,13 @@
+# Создать лямбда-выражения для нахождения площадей фигур
+
+from math import pi
+
+area = {
+    'circle': lambda r: pi * (r ** 2),
+    'rectangle': lambda x, y: x * y,
+    'trapezoid': lambda a, b, h: (a + b) / 2 * h
+}
+
+print("Площадь окружности:", area['circle'](2))
+print("Площадь прямоугольника:", area['rectangle'](10, 13))
+print("Площадь трапеции:", area['trapezoid'](7, 5, 3))
