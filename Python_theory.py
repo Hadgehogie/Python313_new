6d87d7b42138c3c9805b9686b15cb52ef8aa8200
Hadgehogie <test@mail.ru>
2024-07-05 16:21:31 +0300

    added theory

--- Python_theory.py
+++ Python_theory.py
@@ -3165,3 +3165,142 @@ import re
 # print(re.findall(reg, text))  # ['python']
 # print(re.findall(reg1, text))  # ['Python']
 # print(re.findall(reg2, text))  # ['Python', 'python', 'PYTHON']
+
+
+# -----------------------------------------------------
+# Жадное и ленивое соответствие регулярных выражений
+
+# text = "<body>Пример жадного соответствия регулярных выражений</body>"
+# print(re.findall("<.*>", text))
+# print(re.findall("<.*?>", text))  # <body>, </body>
+#
+# # *?, +?, ??, {m,n}?, {,n}?, {m,}?
+#
+# st = "12 сентября 2021 года 1454691600"
+# reg = r"\d{2,4}"  # разбиение по максимуму (4)
+# req = r"\d{2,4}?"  # разбиение по минимуму (2)
+# req1 = r"\d{2,}?"
+#
+# print(re.findall(reg, st))
+# print(re.findall(req, st))
+# print(re.findall(req1, st))
+
+# s = "<p>Изображение <img  src='bg.jpg'  alt='картинка'> - фон страницы</p>"
+# reg = r"<img.*>"  # <img src='bg.jpg'> - фон страницы</p>
+# req = r"<img.*?>"  # <img src='bg.jpg'>
+# req1 = r"<img[^>]*>"  # <img src='bg.jpg'>
+# req2 = r"<img\s+[^>]*src\s*=\s*[^>]+>"  # верная работа кода HTML
+#
+# print(re.findall(req, s))
+# print(re.findall(req1, s))
+# print(re.findall(req2, s))
+
+# text = "Python (в русском языке встречаются названия питон[16] или пайтон[17]) - высокоуровневый язык " \
+#         "программирования общего назначения с динамической строгой типизаций " \
+#         "и автоматическим управлением памятью[18][19]."
+# reg = r"\[\d+]"
+# reg1 = r"\[.*?]"
+#
+# print(re.findall(reg, text))
+# print(re.findall(reg1, text))
+
+# s = "Петр, Ольга и Виталий отлично учатся!"
+# reg = 'Петр|Виктор|Ольга'
+#
+# print(re.findall(reg, s))
+
+# s = "int = 4, float = 4.0f, double = 8.0"
+# reg = r'\w+\s*=\s*\d[.\w]*'
+# reg1 = r'int\s*=\s*\d[.\w]*'
+# reg2 = r'int\s*=\s*\d[.\w]*|float\s*=\s*\d[.\w]*'
+# reg3 = r'(int|float)\s*=\s*\d[.\w]*'
+# reg4 = r'(?:int|float)\s*=\s*\d[.\w]*'
+#
+# print(re.findall(reg, s))
+# print(re.findall(reg1, s))  # ['int = 4']
+# print(re.findall(reg2, s))  # ['int = 4', 'float = 4.0f']
+# print(re.findall(reg3, s))  # ['int', 'float']
+# print(re.findall(reg4, s))  # ['int = 4', 'float = 4.0f']
+#
+#
+# # (?:...) -> группирующие скобки перестают быть сохраняющими (актуально только для метода findall)
+
+# s = '127.0.0.1'
+# s1 = '127.168.255.255'
+#
+# reg = r'\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}'
+# req = r'(?:\d{1,3}.){3}\d{1,3}'
+#
+# print(re.findall(reg, s))
+# print(re.findall(reg, s1))
+# print(re.findall(req, s))
+# print(re.findall(req, s1))
+
+# s = "5 + 7*2 - 4"
+# reg = r'\s*[+*-]\s*'
+# reg1 = r'\s*([+*-])\s*'
+#
+# print(re.split(reg, s))  # ['5', '7', '2', '4']
+# print(re.split(reg1, s))  # ['5', '+', '7', '*', '2', '-', '4']
+
+# date = '28-08-2021'
+# pattern = r'(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(19[0-9][0-9]|20[01][0-9]|202[0-4])'
+#
+# print(re.findall(pattern, date))
+# print(re.search(pattern, date).group())
+
+# s = "Я ищу совпадения в 2024 году. И я их найду в 2 счёта."
+# reg = r'[0-9]+\s\D+'
+# reg1 = r'([0-9]+)\s(\D+)'
+#
+# print(re.findall(reg, s))
+# # print(re.search(reg, s).group())
+# # print(re.search(reg, s)[0])
+# print(re.search(reg, s).group(0))
+# print(re.findall(reg1, s))
+# m = re.search(reg1, s)
+# print(m)
+# print(m[0])
+# print(m[1])
+# print(m[2])
+
+# text = """
+# Самара
+# Москва
+# Тверь
+# Уфа
+# Казань
+# """
+# count = 0
+#
+#
+# def repl_find(m):
+#     global count
+#     count += 1
+#     return f"<option value='{count}'>{m.group(1)}</option>\n"
+#
+#
+# print(re.sub(r'\s*(\w+)\s*', repl_find, text))
+
+# s = "<p>Изображение <img src=\"bg.jpg\"> - фон страницы </p>"
+# reg = r"<img\s+[^>]*src\s*=['\"](.+?)['\"]>"
+# reg1 = r"<img\s+[^>]*src\s*=(['\"])(.+?)\1>"
+# reg2 = r"<(img)\s+[^>]*src\s*=(?P<q>['\"])(.+?)(?P=q)>"
+#
+# print(re.findall(reg, s))
+# print(re.findall(reg1, s))
+# print(re.findall(reg2, s))
+
+
+# (?P<name>...)  (?P=name) - обращение к круглой скобке по имени
+
+# s = "Самолет прилетает 10/23/2024. Будем рады вас видеть после 10/24/2024"  # 23.10.2024
+# reg = r'(?P<mon>\d{2})/(?P<day>\d{2})/(?P<year>\d{4})'
+#
+# print(re.sub(reg, r'\2.\1.\3', s))
+# # print(re.sub(reg, r'(?P=day).(?P=mon).(?P=year)', s))
+
+s = "yandex.com and yandex.ru"
+reg = r'(([a-z0-9-]{2,}\.)+[a-z]{2,4})'
+
+print(re.sub(reg, r'http://\1', s, re.IGNORECASE))
