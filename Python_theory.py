54efaf0e985393877096730c65c39eba0498f408
Hadgehogie <test@mail.ru>
2024-08-11 16:37:51 +0300

    added theory

--- Python_theory.py
+++ Python_theory.py
@@ -6264,18 +6264,156 @@
 #     write_json(gen_person())
 
 
+# import json
+#
+#
+# class Student:
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+#
+#     def __str__(self):
+#         a = ", ".join(map(str, self.marks))
+#         return f'Студент: {self.name}: {a}'
+#
+#     def add_mark(self, mark):
+#         self.marks.append(mark)
+#
+#     def delete_mark(self, index):
+#         self.marks.pop(index)
+#
+#     def edit_mark(self, index, new_mark):
+#         self.marks[index] = new_mark
+#
+#     def average_mark(self):
+#         return round(sum(self.marks) / len(self.marks), 2)
+#
+#     def dump_to_json(self):
+#         data = {'name': self.name, 'marks': self.marks}
+#         with open(self.get_file_name(), 'w') as f:
+#             json.dump(data, f)
+#
+#     def load_from_file(self):
+#         with open(self.get_file_name(), 'r') as f:
+#             print(json.load(f))
+#
+#     def get_file_name(self):
+#         return f'{self.name}.json'
+#
+#
+# class Group:
+#     def __init__(self, students, group: str):
+#         self.students = students
+#         self.group: str = group
+#
+#     def __str__(self):
+#         a = " \n".join(map(str, self.students))
+#         return f'\nГруппа: {self.group} \n{a}'
+#
+#     @staticmethod
+#     def change_group(group_1, group_2, index):
+#         group_2.add_student(group_1.delete_student(index))
+#
+#     def add_student(self, new_student):
+#         self.students.append(new_student)
+#
+#     def delete_student(self, index):
+#         return self.students.pop(index)
+#
+#     def dump_to_json(self):
+#         data = [
+#                 {'name': student.name, 'marks': student.marks} for student in self.students
+#             ]
+#         with open(self.get_file_name(), 'w') as f:
+#             json.dump(data, f, indent=2)
+#
+#     def get_file_name(self):
+#         return f'{self.group.lower().replace(" ", "-")}.json'
+#
+#     def load_from_file(self):
+#         with open(self.get_file_name(), 'r') as f:
+#             print(json.load(f))
+#
+#
+# st1 = Student('Bodnya', [5, 4, 3, 4, 5, 3])
+# st2 = Student('Nikolaenko', [2, 3, 5, 4, 2])
+# st3 = Student('Birukov', [3, 5, 3, 2, 5, 4])
+#
+# sts1 = [st1, st2]
+#
+# group1 = Group(sts1, "ГК Python")
+# # # print(f'{group1} \n')
+#
+# group1.add_student(st3)
+# # # print(f'{group1} \n')
+#
+# group1.delete_student(1)
+# # print(group1)
+#
+# sts2 = [st2]
+#
+# group2 = Group(sts2, "ГК Web")
+# # print(group2)
+#
+# Group.change_group(group1, group2, 0)
+# # print(group1)
+# # print(group2)
+#
+# group2.dump_to_json()
+# group2.load_from_file()
+
+# print(st1)
+#
+# st1.add_mark(4)
+# print(st1)
+#
+# st1.delete_mark(2)
+# print(st1)
+#
+# st1.edit_mark(4, 5)
+# print(st1)
+#
+# print("Средний балл студента: ", st1.average_mark())
+
+# st1.dump_to_json()
+# st1.load_from_file()
+
+
 import json
 
 
-class Student:
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
+class CountryCapital:
+    @staticmethod
+    def add_country(file_name):
+        new_country = input("Введите название страны: ").lower()
+        new_capital = input("Введите название столицы: ").lower()
 
-    def __str__(self):
-        a = ", ".join(map(str, self.marks))
-        return f'Студент: {self.name}: {a}'
+        try:
+            data = json.load(open(file_name))
+        except FileNotFoundError:
+            data = {}
+
+        data[new_country] = new_capital
+
+        with open(file_name, 'w') as f:
+            json.dump(data, f, indent=2, ensure_ascii=False)
+
+    @staticmethod
+    def load_from_file(file_name):
+        with open(file_name) as f:
+            print({k.capitalize(): v.capitalize() for k, v in json.load(f).items()})
 
 
-st1 = Student('Bodnya', [5, 4, 3, 4, 5, 3])
-print(st1)
+file = "list_capital.json"
+while True:
+    index = input("Выбор действия: \n1 - добавление данных \n2 - удаление данных \n3 - поиск данных \n4 - "
+                  "редактирование данных \n5 - просмотр данных \n6 - завершение работы \nВвод: ")
+
+    if index == '1':
+        CountryCapital.add_country(file)
+    elif index == '5':
+        CountryCapital.load_from_file(file)
+    elif index == '6':
+        break
+    else:
+        print("Введено некорректное значение")
